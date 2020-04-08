919ae34e29968b40b3975f2b3619882f528a8dea
Rui Oliveira <ruimail24@gmail.com>
2020-04-08 17:12:29 +0100

    Defined VScode task

---
+++ configure.bat
@@ -0,0 +1,2 @@
+call activate extension-dev37
+D:\Programas\CMake\bin\cmake.EXE --no-warn-unused-cli -DCMAKE_EXPORT_COMPILE_COMMANDS:BOOL=TRUE -DCMAKE_BUILD_TYPE:STRING=Debug -DCMAKE_C_COMPILER:FILEPATH=D:\Programas\msys2\mingw64\bin\gcc.exe -DCMAKE_CXX_COMPILER:FILEPATH=D:\Programas\msys2\mingw64\bin\g++.exe -Hd:/User/Gdrive/Documentos/Code/Python/liquid-dsp-swig -Bd:/User/Gdrive/Documentos/Code/Python/liquid-dsp-swig/build/Debug -G Ninja
